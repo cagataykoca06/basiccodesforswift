08fa39ac7e487072f2725bc6921ce381ed13d8a9
cagatay <142677193+cagataykoca06@users.noreply.github.com>
2025-09-11 15:55:25 +0300

    Add files via upload

---
+++ alphabetExample.swift
@@ -0,0 +1,7 @@
+var consonants: Set = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"]
+
+var vowels: Set = ["A", "E", "I", "O", "U"]
+
+
+var alphabet = consonants.union(vowels)
+print(alphabet)
