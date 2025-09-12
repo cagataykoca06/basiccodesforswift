64ec421a907185b6aaceffa69032cd1a17bd03d9
cagatay <142677193+cagataykoca06@users.noreply.github.com>
2025-09-12 09:22:58 +0300

    Add files via upload

---
+++ destination.swift
@@ -0,0 +1,8 @@
+
+func timeToDestination(distance: Int, speed: Int) -> Int {
+  let time = distance / speed
+  return time
+}
+
+print(timeToDestination(distance: 6836, speed: 560))
+
---
+++ findCircumference.swift
@@ -0,0 +1,7 @@
+
+func findCircumference(diameter: Double) -> Double {
+  return 3.14 * diameter
+}
+
+var result = findCircumference(diameter: 5.5)
+print(result)
