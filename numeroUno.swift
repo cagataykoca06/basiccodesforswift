3b4a6d8c3ce6fde0344a9b1bf386f0cdcdec95fc
cagatay <142677193+cagataykoca06@users.noreply.github.com>
2025-09-21 19:18:38 +0300

    Add files via upload

---
+++ numeroUno.swift
@@ -0,0 +1,81 @@
+enum ProductType: String, CaseIterable {
+  case aceRepository = "Ace Repository"
+  case dealForcer = "Deal Forcer"
+  case kadencePlanner = "Kadence Planner"
+  case mailCannon = "Mail Cannon"
+}
+
+func displayProductOfferings() {
+  print("There are \(ProductType.allCases.count) products: ")
+  for product in ProductType.allCases {
+    print(product.rawValue)
+  }
+}
+
+
+enum Edition: String {
+  case basic
+  case premium
+  case ultimate
+
+
+  mutating func upgrade() {
+    switch self {
+      case .basic:
+        self = .premium
+      case .premium:
+        self = .ultimate
+      case .ultimate:
+        print("Can't upgrade further")
+    }
+  }
+  
+}
+
+
+
+enum DeliveryMethod {
+  case cloudDigital(isLifetime: Bool)
+  case shipping(weight: Int)
+
+  var shippingCost: Int {
+    switch self {
+      case .shipping(let weight):
+        return weight * 2
+      case .cloudDigital:
+        return 0.0
+    }
+  }
+}
+
+
+
+func sendOrderConfirmation(of productType: ProductType, and edition: Edition, using deliveryMethod: DeliveryMethod) {
+
+  print("Thank you for purchasing the \(edition.rawValue) edition of \(productType.rawValue)")
+
+  switch deliveryMethod {
+    case .shipping:
+      print("Your order will be shipped to you at a cost of $\(deliveryMethod.shippingCost)")
+  case .cloudDigital(let isLifetime):
+    if isLifetime {
+      print("You have lifetime cloud access")
+    }else {
+      print("You can access your subscription information in the cloud.")
+    }
+    
+  }
+}
+
+getProductOfferings()
+
+var myEdition = Edition.basic
+myEdition.upgrade()
+
+sendOrderConfirmation(of: ProductType.aceRepository, and: myEdition, using: DeliveryMethod.shipping(weight: 1))
+
+
+
+
+
+
