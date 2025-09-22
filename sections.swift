e8455b0a4502a94bb0f097dce0610f740ddb2b48
cagatay <142677193+cagataykoca06@users.noreply.github.com>
2025-09-22 13:26:53 +0300

    Add files via upload

---
+++ sections.swift
@@ -0,0 +1,11 @@
+List {
+    Section(header: Text("Designers")) {
+        Text("Catherine")
+        Text("Ahmed")
+    }
+    Section(header: Text("Developers")) {
+        Text("Chen")
+        Text("Serena")
+        Text("Alexis")
+    }
+}
