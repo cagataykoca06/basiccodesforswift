3db914c5dbd105a6f6bfc1a0bfcf3aa995608acd
cagatay <142677193+cagataykoca06@users.noreply.github.com>
2025-10-13 15:31:06 +0300

    Add files via upload

---
+++ acyclicGraph.swift
@@ -0,0 +1,93 @@
+class GraphNode {
+    
+  var data: String
+  var neighboringNodes: [GraphNode]
+  
+  init(data: String) {
+      self.data = data
+      self.neighboringNodes = []
+  }
+  
+  func addNeighbor(_ newNeighbor: GraphNode) {
+      neighboringNodes.append(newNeighbor)
+  }
+  
+  func removeNeighbor(_ nodeToRemove: GraphNode) {
+    if let index = neighboringNodes.firstIndex(where: { $0 == nodeToRemove }) {
+      neighboringNodes.remove(at: index)
+    }
+  }
+}
+extension GraphNode: Equatable {
+  static func == (lhs: GraphNode, rhs: GraphNode) -> Bool {
+      return lhs === rhs
+  }
+}
+extension GraphNode: CustomStringConvertible {
+  var description: String {
+      return "\(data)"
+  }
+}
+
+struct GraphEdge {
+  let nodeOne: GraphNode
+  let nodeTwo: GraphNode
+  
+  init(nodeOne: GraphNode, nodeTwo: GraphNode) {
+    self.nodeOne = nodeOne
+    self.nodeTwo = nodeTwo
+  }
+}
+
+class Graph {
+  var nodes: [GraphNode]
+  var edges: [GraphEdge]
+    
+  init(nodes: [GraphNode]) {
+    self.nodes = nodes
+    edges = []
+  }
+
+  func addEdge(from nodeOne: GraphNode, to nodeTwo: GraphNode) {
+    edges.append(GraphEdge(nodeOne: nodeOne, nodeTwo: nodeTwo))
+    nodeOne.addNeighbor(nodeTwo)
+  }
+  
+  func addEdges(from nodeOne: GraphNode, to neighboringNodes: [GraphNode]) {
+    for node in neighboringNodes {
+      addEdge(from: nodeOne, to: node)
+    }
+  }
+
+  func removeNode(_ node: GraphNode) {
+    if let index = nodes.firstIndex(where: { $0 == node }) {
+      nodes.remove(at: index)
+    }
+    
+    edges = edges.filter { $0.nodeOne != node || $0.nodeTwo != node }
+    nodes.forEach { $0.removeNeighbor(node) }
+  }
+  
+  func printGraph() {
+    for node in nodes {
+      Swift.print("\(node): \(node.neighboringNodes)")
+    }
+  }
+}
+
+// Datas are here
+let car = GraphNode(data: "Car")
+let bus = GraphNode(data: "Bus") 
+let gasStation = GraphNode(data: "Gas Station")
+let busStop = GraphNode(data: "Bus Stop")
+let work = GraphNode(data: "Work")
+
+let graph = Graph(nodes: [car, bus, gasStation, busStop, work])
+
+graph.addEdge(from: car, to: gasStation)
+graph.addEdge(from: gasStation, to: work)
+graph.addEdge(from: bus, to: busStop)
+graph.addEdge(from: busStop, to: work)
+
+graph.printGraph()
+
